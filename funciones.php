61fdbd3939c4bf03ed9b87b5bd69952eb0968a41
lzsdjuan2119 <sedanojuan350@gmail.com>
2026-05-08 09:10:44 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.cookie_secure', 1); // Obligatorio para HTTPS de Railway
